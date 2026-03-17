db7aea4b53f0351ff0e1fbba841e9a1216e970d3
CamilaBrozeguine <camilabrozeguine@gmail.com>
2026-03-17 13:54:55 -0400

    importação de novas fonts

---
+++ my-app/app/(tabs)/signup.tsx
@@ -0,0 +1,89 @@
+import { Input } from "@/components/input";
+import { Link } from "expo-router";
+import { Button } from "@/components/Button"
+import { StyleSheet, KeyboardAvoidingView, ScrollView, Image } from 'react-native';
+import { useState } from "react";
+import {Text, View, Alert, Platform, } from 'react-native';
+import React from "react";
+
+
+export default function Signup() {
+
+
+    return(
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior= {Platform.select({ ios: "padding", android: undefined })}>
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }}keyboardShouldPersistTaps="handled">
+                <View style={styles.container}>
+       
+   
+            <Image
+            source={require("@/assets/images/illustration of woman character with phone (4).jpg")}
+            style={styles.illustration}
+            />
+            <Text style={styles.title}>Criar conta</Text>
+            <Text style={styles.subtitle}>Crie sua conta para acessar</Text>
+
+
+                <View style={styles.form}>
+                <Input placeholder="Nome" />
+                <Input placeholder="E-mail" keyboardType='email-address'/>
+                <Input placeholder="Senha" secureTextEntry />
+                <Input placeholder="Confirmar Senha" secureTextEntry />
+                <Button title="Criar conta" onPress={() => alert("Cadastrou")}/>
+                </View>
+                <Text style={styles.footertext}>
+                    Já tem uma conta? {""}
+                    <Link href="/" style ={ styles.link}
+                    >Entrar</Link>
+                </Text>
+            </View>
+    </ScrollView>
+    </KeyboardAvoidingView>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+       backgroundColor: "#fdfdf0",
+       padding: 32,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    subtitle:{
+        fontSize: 16,
+        marginTop: 8,
+    },
+
+
+    illustration: {
+        width: "80%",
+        height: 200,
+        resizeMode: "contain",
+        marginTop: 62,
+        alignContent: "center",
+        justifyContent: "center",
+       
+    },
+    form: {
+        marginTop: 24,
+        gap: 12,
+
+
+    },
+    footertext: {
+        marginTop: 24,
+        textAlign: "center",
+        color: "#712e91",
+    },
+    link: {
+        color: "#673496",
+        fontWeight: "bold",
+     
+        paddingHorizontal: 8,
+        paddingVertical: 4,
+        borderRadius: 4,
+    }
+});
+
